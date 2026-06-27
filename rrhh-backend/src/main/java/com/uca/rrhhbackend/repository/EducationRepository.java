43ccc9dea776ef42b88ecd24ca7c19ac8b5a25a7
eliuthlarios <00029223@uca.edu.sv>
2026-06-27 16:56:52 -0600

    adding candidate repository methods

--- rrhh-backend/src/main/java/com/uca/rrhhbackend/repository/EducationRepository.java
+++ rrhh-backend/src/main/java/com/uca/rrhhbackend/repository/EducationRepository.java
@@ -4,8 +4,14 @@ import com.uca.rrhhbackend.entity.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EducationRepository extends JpaRepository<Education, Long> {
 
     List<Education> findByCandidateProfileId(Long candidateProfileId);
+
+
+    Optional<Education> findByIdAndCandidateProfileId(
+            Long id,
+            Long candidateProfileId );
 }
--- rrhh-backend/src/main/java/com/uca/rrhhbackend/repository/WorkExperienceRepository.java
+++ rrhh-backend/src/main/java/com/uca/rrhhbackend/repository/WorkExperienceRepository.java
@@ -4,9 +4,15 @@ import com.uca.rrhhbackend.entity.WorkExperience;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkExperienceRepository
         extends JpaRepository<WorkExperience, Long> {
 
     List<WorkExperience> findByCandidateProfileId(Long candidateProfileId);
+
+    Optional<WorkExperience> findByIdAndCandidateProfileId(
+            Long id,
+            Long candidateProfileId
+    );
 }
